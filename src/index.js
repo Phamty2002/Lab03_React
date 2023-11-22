64e85e7617d8341958e508484281283993f7e6c5
phamty <114122026+Phamty2002@users.noreply.github.com>
2023-11-22 09:59:07 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import MySection from "./MySection";
+import MyButton from "./MyButton";
+import MyComponent from "./MyComponent";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MySection>
+    <MyButton>My Button Text</MyButton>
+    <MyComponent />
+  </MySection>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
